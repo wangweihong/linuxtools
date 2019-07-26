35b108676c7ca5b30592151fddfa39e348335f01
wangweihong <wangweihong2018@qq.com>
2019-07-26 14:01:29 +0800

    fix install golang

--- golang/install-golang.sh
+++ golang/install-golang.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+set -e
 
 release=1.11.1
 targz=go$release.linux-amd64.tar.gz
 
-if [ -ne $GOROOT ];then
+if [ -z $GOROOT ];then
    echo "must set GOROOT env"
    exit -1
 fi 
 
-if [ -d $GOROOT ];then
+if [ ! -d $GOROOT ];then
    echo "GOROOT:$GOROOT path not exist"
    exit -1
 fi 
 
 wget https://dl.google.com/go/$targz
-tar -xvzf targz -C /tmp
-mv /tmp/go $GOROOT
-
-
-
+tar -xvzf $targz -C /tmp
+cp -rf /tmp/go/* $GOROOT/
+go version
